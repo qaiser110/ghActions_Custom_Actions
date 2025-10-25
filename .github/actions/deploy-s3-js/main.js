52e0200e05681e2552ec2aba964df6de95de8580
Qaiser <qaiser110@gmail.com>
2025-10-25 23:06:36 +1100

    Action Outputs

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -12,7 +12,11 @@ async function run() {
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`);
 
+  const websiteUrl = `http://${bucket}.s3-website-${region}.amazonaws.com`;
+  core.setOutput("deployed-url", websiteUrl); // ::set-output
+
   core.notice(`Deploying from ${distFolder} to ${s3Uri} in region ${region}`);
+  core.notice(`Site URL ${websiteUrl}`);
 }
 
 run();
